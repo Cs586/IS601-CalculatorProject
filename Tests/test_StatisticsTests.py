e8388c6d30ca4a20137e45d849f41bfd93eb7e5e
hl533 <hl533@njit.edu>
2019-11-07 18:38:35 -0500

    TASK: Fixed test_StatisticsTests.py statistics filepath reference, so that error is fixed. New errors now.

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -6,16 +6,17 @@ from Statistics.Statistics import Statistics
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.statistics = Statistics()
+        self.statistics = Statistics('Tests/Data/UnitTestStats.csv')
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
     def test_mean_statistics(self):
         test_data = CsvReader('Tests/Data/UnitTestStats.csv').data
-        for row in test_data:
-            result = 'mean'
-            self.assertEqual(self.statistics.mean(self.data, result))
+        answers = CsvReader('Tests/Data/UnitTestStatsAnswers.csv').data
+        for column in test_data:
+            result = self.answers(float(column['mean']))
+            self.assertEqual(self.statistics.mean['Value 1'], result)
 
 
 if __name__ == '__main__':
