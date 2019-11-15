b384358885a1d593bc7192be9b0f6b833a3ca892
hl533 <hl533@njit.edu>
2019-11-15 11:14:47 -0500

    EDIT: Median test running now

--- Statistics/Median.py
+++ Statistics/Median.py
@@ -6,20 +6,17 @@ from Calculator.Addition import addition
 def median(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t):
     try:
         median_list = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t]
-        # median_list = [int(i) for i in median_list]
+        float_list = [float(i) for i in median_list]
         num_values = len(median_list)
-        median_list.sort()
+        float_list.sort()
         if num_values % 2 == 0:
-            median1 = median_list[int(num_values // 2)]
-            median2 = median_list[int(subtraction((num_values // 2), 1))]
-            return division(addition(median1, median2), 2)
+            median1 = float_list[int(num_values // 2)]
+            median2 = float_list[int(subtraction((num_values // 2), 1))]
+            median_result = division(addition(median1, median2), 2)
         else:
-            return median_list[division(num_values, 2)]
+            median_result = float_list[int(division(num_values, 2))]
+        return median_result
     except ZeroDivisionError:
         print("Error: Can't Divide by 0")
     except ValueError:
-        print ("Error: Check your data inputs")
-
-# n = len(n_num)
-#
-# n_num.sort()
+        print("Error: Check your data inputs")
--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -37,10 +37,10 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             result = float(row['Median'])
             self.assertEqual(self.statistics.median(row['Value 1'], row['Value 2'], row['Value 3'], row['Value 4'],
-                                                  row['Value 5'], row['Value 6'], row['Value 7'], row['Value 8'],
-                                                  row['Value 9'], row['Value 10'], row['Value 11'], row['Value 12'],
-                                                  row['Value 13'], row['Value 14'], row['Value 15'], row['Value 16'],
-                                                  row['Value 17'], row['Value 18'], row['Value 19'], row['Value 20']),
+                                                    row['Value 5'], row['Value 6'], row['Value 7'], row['Value 8'],
+                                                    row['Value 9'], row['Value 10'], row['Value 11'], row['Value 12'],
+                                                    row['Value 13'], row['Value 14'], row['Value 15'], row['Value 16'],
+                                                    row['Value 17'], row['Value 18'], row['Value 19'], row['Value 20']),
                              result)
             self.assertEqual(self.statistics.result, result)
         pprint(test_data)
